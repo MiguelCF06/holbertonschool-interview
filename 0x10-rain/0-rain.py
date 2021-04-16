8564e557a87e93ca502586581d76dc29ac7bd7e7
MiguelCF06 <miguel.cipamocha.com>
2021-04-15 19:34:37 -0500

    [Update]First Update

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Given a list of non-negative integers representing walls of width 1,
+calculate how much water will be retained after it rains.
+"""
+
+
+def rain(walls):
+    """
+    Parameters:
+        walls: A list of non-negative integers
+    Returns:
+        int: The total amount of rain water that were collected
+    """
+    if walls is None:
+        return 0
+
+    walls_length = len(walls)
+    max_water = 0
+
+    if walls == 0 or walls_length == 1:
+        return 0
+
+    for i in range(1, walls_length - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, walls_length):
+            right = max(right, walls[j])
+        max_water = max_water + (min(left, right) - walls[i])
+    return max_water
