c01ba9ca328b90513ccb1cc021382f7568d3f45e
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-04-29 16:18:07 +0000

    [Update]First Update

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(int *array, size_t size);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H */
