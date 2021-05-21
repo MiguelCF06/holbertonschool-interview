e6955b69e97680a41e5486ac1530928d37597431
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-05-21 15:15:43 +0000

    [Update]First Update

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void _error(void);
+void check_args(int argc, char **argv);
+void write_to_file(char **argv);
+void multiply(void);
+
+#endif /* _HOLBERTON_H */
