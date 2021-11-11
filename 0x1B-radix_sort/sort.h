b6db9650a493fc79cc029c79a2350bda4c59ae71
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-11-11 18:25:48 +0000

    [Updated]First Update

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_H
+#define _SORT_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* _SORT_H */
